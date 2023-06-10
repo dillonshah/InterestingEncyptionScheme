de3fe27d2b9e63b34f8c5b4072fae71c17dcbe8b
elliotvm <68871718+elliotvm@users.noreply.github.com>
2023-06-10 19:06:17 -0400

    Fixed timeStamp

--- server.py
+++ server.py
@@ -31,30 +31,32 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             # Receive IV
             iv = conn.recv(1024)
             print("IV Received")
+
             # Receive message
             data = conn.recv(4096)
             print("Recieved message")
+
             # Split message on BREAK constant, and decrypt
-            encrypted_message, hmac, sig, timeStamp = data.split(b"0000")
-            msg = decryptAES(key, encrypted_message, iv)
+            ct, hmac, timeStamp, sig = data.split(b"0000")
+
+            msg = decryptAES(key, ct, iv)
             print("Unencrypted message: ", msg)
 
             # Verify that HMACs work
-            newHmac = getHMAC_SHA256(encrypted_message)
+            newHmac = getHMAC_SHA256(ct)
             if newHmac == hmac:
                 print("MACs same, integrity guaranteed")
             else :
                 print("MACS differ, message tampered with")
         
             #Verify signature
-            print(verify(encrypted_message + hmac, sig, "client"))
+            print(verify(ct + hmac + timeStamp, sig, "client"))
 
             #Verify Timestamp
-            rTimestamp = float(timeStamp.decode('utf-8'))
+            rTimestamp = timeStamp.decode('utf-8')
             cTimeStamp = time.time()
-            max_time_difference = 5
 
-            if time.time() - float(timeStamp.decode('utf-8')) <= 5:
+            if time.time() - float(timeStamp.decode('utf-8')) <= 10:
                 print("Time stamp is valid")
                 
             else:
