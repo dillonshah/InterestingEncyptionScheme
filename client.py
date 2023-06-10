9fb28303548c1ebbb5f0811f2ffed85ee1d64037
elliotvm <68871718+elliotvm@users.noreply.github.com>
2023-06-10 19:05:19 -0400

    Fix timestamp and random

--- client.py
+++ client.py
@@ -1,19 +1,19 @@
 import socket
-import os
 import time
 
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
 from keyGenerator import getKeys
-from aes import encryptAES, decryptAES
+from aes import encryptAES
 from mac import getHMAC_SHA256
 from signature import signature
 
+import secrets
+
 HOST = "127.0.0.1" 
 PORT = 65412
-
-KEY = os.urandom(32)
+KEY = secrets.token_bytes(32)
 
 MESSAGE = b"It\'s not a bug, it\'s a feature."
 BREAK = b"0000"
@@ -27,10 +27,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
     # Get server's public key
     with open("keys/pub_server.pem","r") as f:
-        server_public_key = RSA.importKey(f.read())
+        pubKey = RSA.importKey(f.read())
     
     # Encrypt session key using server's public key
-    encryptor = PKCS1_OAEP.new(server_public_key)
+    encryptor = PKCS1_OAEP.new(pubKey)
     ek = encryptor.encrypt(KEY)
     s.sendall(ek)
 
@@ -39,17 +39,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         # Encrypt message
         iv, ct = encryptAES(KEY, MESSAGE)
         print("Message encrypted")
+
         s.sendall(iv)
         # Generate HMAC
         hmac = getHMAC_SHA256(ct)
         print("HMAC applied")
-        # Generate signature of concatanated message and hmac
-        sig = signature(ct + hmac, 2048, "client")
-        print("Signature Calculated, sending Message")
-        
+
+        #generate timestamp
         timestamp = str(time.time()).encode('utf-8')
 
-        # Send result to server
-        s.sendall(ct + BREAK + hmac + BREAK + sig + BREAK + timestamp)
-
+        # Generate signature of concatanated message and hmac
+        sig = signature(ct + hmac + timestamp, 2048, "client")
+        print("Signature Calculated, sending Message")
         
+
+        # Send result to server
+        s.sendall(ct + BREAK + hmac + BREAK + timestamp + BREAK + sig)
